7745e09f11c883035bf0ddd52246d54575233d28
Sashkewic <sashkewicha@gmail.com>
2022-02-19 14:27:06 +0600

    Созданы первые компоненты для todo-приложения - форма и навигационная панель. Созданы две страницы - главная и информация о приложении

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+export const About = () =>
+(
+    <div className="jumbotron bg-light p-5">
+        <div className="container py-5">
+            <h1 className="display-4">Todo List App with Bootstrap 5</h1>
+            <p className="lead">Application version - <strong>1.0.0</strong></p>
+        </div>
+    </div>
+)
