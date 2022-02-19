1fa3e221694d45194a1bbc7ee55811ce5abf9881
Sashkewic <sashkewicha@gmail.com>
2022-02-19 16:15:33 +0600

    Добавлен компонент с заметкой (notes), добавлен display: flex к элементам списка заметок. Добавлена дата создания заметки (на начальной стадии)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from "react";
+import { Form } from "../components/Form";
+import { Notes } from "../components/Notes";
 
 export const Home = () => {
+    const notes = new Array(3).fill('').map((_, i) => ({id: 1, title: `Note ${i + 1}`}))
+
     return (
         <Fragment>
-            <h1>Home Page</h1>
+            <Form />
+            <hr />
+            <Notes notes={notes}/>
         </Fragment>
     )
 }
